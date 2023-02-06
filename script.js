2e59f9e77aebff05ffaa228ca0f3ca1065188b01
TreasureUzoma <123127790+TreasureUzoma@users.noreply.github.com>
2023-02-07 00:31:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,89 +14,70 @@ function menu(){
  y.style.display ="block"
  var z = document.getElementById("mc")
  z.style.display ="none"
- }
- function convertToRoman() {
- var n1 = document.getElementById("n1")
- var num = document.getElementById("n1").value;
- if(num>5000) { paragraph = document.getElementById("r1");
- alert("Tge numner must be less than 5000")
- }
- else {
- var my = [];
+ }function convertToRoman() {
+ const number = document.getElementById("n1").value;
+ let romanNumeral = "";
+ const romanNumerals = [
+ "M",
+ "CM",
+ "D",
+ "CD",
+ "C",
+ "XC",
+ "L",
+ "XL",
+ "X",
+ "IX",
+ "V",
+ "IV",
+ "I"
+ ];
+ const numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
  
- var M = Math.floor(num / 1000);
- for (i = 1; i <= M; i++) {
- my.push("M");
+ for (let i = 0; i < numbers.length; i++) {
+ while (number >= numbers[i]) {
+ romanNumeral += romanNumerals[i];
+ number -= numbers[i];
  }
- var D = Math.floor((num % 1000) / 500);
- for (i = 1; i <= D; i++) {
- my.push("D");
- }
- var C = Math.floor((num % 500) / 100);
- for (i = 1; i <= C; i++) {
- my.push("C");
- }
- var L = Math.floor((num % 100) / 50);
- for (i = 1; i <= L; i++) {
- my.push("L");
- }
- 
- var X = Math.floor((num % 50) / 10);
- for (i = 1; i <= X; i++) {
- my.push("X");
  }
  
- var V = Math.floor((num % 10) / 5);
- for (i = 1; i <= V; i++) {
- my.push("V");
+ document.getElementById("r1").value = romanNumeral;
  }
  
- var I = num % 5;
- for (i = 1; i <= I; i++) {
- my.push("I");
+ function convert2() {
+ let roman = document.getElementById("r2").value;
+ let result = 0;
+ 
+ let romanNumeralMap = {
+ 'I': 1,
+ 'IV': 4,
+ 'V': 5,
+ 'IX': 9,
+ 'X': 10,
+ 'XL': 40,
+ 'L': 50,
+ 'XC': 90,
+ 'C': 100,
+ 'CD': 400,
+ 'D': 500,
+ 'CM': 900,
+ 'M': 1000
+ };
+ 
+ for (let i = 0; i < roman.length; i++) {
+ let char = roman[i];
+ let nextChar = roman[i + 1];
+ let charValue = romanNumeralMap[char];
+ let nextCharValue = romanNumeralMap[nextChar];
+ 
+ if (nextCharValue > charValue) {
+ result += nextCharValue - charValue;
+ i++;
+ } else {
+ result += charValue;
+ }
  }
  
- 
- for(i=0;i<my.length;i++) {
- 
- 
- deletefour(my,i,"I","V") ;
- deletefour(my,i,"X","L") ;
- deletefour(my,i,"C","D") ;
- 
- deletefive(my,i,"V","I","X");
- deletefive(my,i,"L","X","C");
- deletefive(my,i,"D","C","M");
- 
- }
- paragraph = document.getElementById("r1");
- paragraph.value = my.join('');
- 
- } }
- 
- function deletefour(arr,i,a,b) {
- 
- if ((arr[i]==a)&&(arr[i+1]==a)&&(arr[i+2]==a)&&(arr[i+3]==a)) {
- arr[i+1]=b;
- arr[i+2]="";
- arr[i+3]="";
- 
- 
+ document.getElementById("n2").value = result;
  }
  
- }
- 
- function deletefive(arr,i,a,b,c) {
- 
- if ((arr[i]==a)&&(arr[i+1]==b)&&(arr[i+2]==b)&&(arr[i+3]==b)&&(arr[i+4]==b)) {
- arr[i]=b;
- arr[i+1]=c;
- arr[i+2]="";
- arr[i+3]="";
- arr[i+4]="";
- 
- 
- 
- }
- 
- }
